0590beb8a76fce69fe94958289167900f70825d5
tr26530es <tr26530es@SESEI69198>
2026-01-22 15:07:29 -0300

    .

--- info_gathering/2.py
+++ info_gathering/2.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-import os  as os
+import os as os
 
 pessoa = {
     "nome": "Gabriel",
     "idade": 23,
     "localidade": "Brasília",
-    "area": "cybersecurity"
+    "area": "cybersecurity",
 }
 
-def input_data() :
+
+def input_data():
     name = str(input("digite seu nome -> "))
     idade = int(input("digite sua idade -> "))
     localidade = str(input("digite sua localidade -> "))
