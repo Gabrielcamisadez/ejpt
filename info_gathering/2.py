0e944e5389ef8357cfe2ec2a2144919e41323e50
Gabrielcamisadez <gabriel448521@gmail.com>
2026-01-22 10:48:12 -0300

    .

--- info_gathering/2.py
+++ info_gathering/2.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+import os  as os
+
+pessoa = {
+    "nome": "Gabriel",
+    "idade": 23,
+    "localidade": "Brasília",
+    "area": "cybersecurity"
+}
+
+def input_data() :
+    name = str(input("digite seu nome -> "))
+    idade = int(input("digite sua idade -> "))
+    localidade = str(input("digite sua localidade -> "))
+    area = str(input("digite sua area -> "))
+
+    return name, idade, localidade, area
+
+
+input_data()
